38134c11d39927858b796146b7e266ad174e4c41
Andreas Zeidler <andreas.zeidler@zeit.de>
2021-04-16 12:35:06 +0200

    Add chezmoi version 1.8.11

---
+++ Formula/chezmoi@1.8.11.rb
@@ -0,0 +1,37 @@
+class ChezmoiAT1811 < Formula
+  desc "Manage your dotfiles across multiple diverse machines, securely"
+  homepage "https://chezmoi.io/"
+  url "https://github.com/twpayne/chezmoi.git",
+      tag:      "v1.8.11",
+      revision: "2e5a76c23de034344f7a9fa89cd3088cd9b52ac7"
+  license "MIT"
+  head "https://github.com/twpayne/chezmoi.git"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{Utils.git_head}
+      -X main.date=#{Time.now.utc.rfc3339}
+      -X main.builtBy=homebrew
+    ].join(" ")
+    system "go", "build", *std_go_args, "-ldflags", ldflags
+
+    bash_completion.install "completions/chezmoi-completion.bash"
+    fish_completion.install "completions/chezmoi.fish"
+    zsh_completion.install "completions/chezmoi.zsh" => "_chezmoi"
+
+    prefix.install_metafiles
+  end
+
+  test do
+    # test version to ensure that version number is embedded in binary
+    assert_match "version #{version}", shell_output("#{bin}/chezmoi --version")
+    assert_match "built by homebrew", shell_output("#{bin}/chezmoi --version")
+
+    system "#{bin}/chezmoi", "init"
+    assert_predicate testpath/".local/share/chezmoi", :exist?
+  end
+end
